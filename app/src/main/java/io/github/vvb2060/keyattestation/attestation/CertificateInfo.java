216322bceac15fa474774d6bf35c0011405a6729
shoey63 <shoey63@users.noreply.github.com>
2026-03-18 17:51:27 +0800

    Update CertificateInfo.java

--- app/src/main/java/io/github/vvb2060/keyattestation/attestation/CertificateInfo.java
+++ app/src/main/java/io/github/vvb2060/keyattestation/attestation/CertificateInfo.java
@@ -1,7 +1,6 @@
 package io.github.vvb2060.keyattestation.attestation;
 
 import android.util.Log;
-import io.github.vvb2060.keyattestation.R;
 
 import java.security.GeneralSecurityException;
 import java.security.PublicKey;
