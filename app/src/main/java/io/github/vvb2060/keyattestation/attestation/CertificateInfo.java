54b501a21b8995c97fd51a83e2aac9074304ad65
shoey63 <shoey63@users.noreply.github.com>
2026-03-18 17:10:34 +0800

    Update CertificateInfo.java

--- app/src/main/java/io/github/vvb2060/keyattestation/attestation/CertificateInfo.java
+++ app/src/main/java/io/github/vvb2060/keyattestation/attestation/CertificateInfo.java
@@ -73,8 +73,24 @@ public class CertificateInfo {
             status = CERT_REVOKED;
             var certStatus = RevocationList.get(cert.getSerialNumber());
             if (certStatus != null) {
-                // Just throw the status message (e.g., "REVOKED") without any suffix
-                throw new CertificateException(certStatus.status());
+                String statusMsg = certStatus.status();
+                String suffix = "";
+
+                // Determine the suffix based on the data source
+                switch (certStatus.source()) {
+                    case CACHE:
+                        suffix = "  " + AppApplication.app.getString(R.string.revocation_status_offline_cached);
+                        break;
+                    case BUNDLED:
+                        suffix = "  " + AppApplication.app.getString(R.string.revocation_status_offline_bundled);
+                        break;
+                    case NETWORK:
+                    default:
+                        break;
+                }
+
+                // Throw the exception with the combined message
+                throw new CertificateException(statusMsg + suffix);
             }
             
             status = CERT_EXPIRED;
